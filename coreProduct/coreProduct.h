e0a8a2256ab7e7e392cea9a13126c0e46d89e8d2
sandipermata <45228664+sandipermata@users.noreply.github.com>
2019-11-28 16:55:24 +0700

    Initial Commit

---
+++ coreProduct/coreProduct.h
@@ -0,0 +1,19 @@
+//
+//  coreProduct.h
+//  coreProduct
+//
+//  Created by Sandi Permata on 28.11.19.
+//  Copyright © 2019 everest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for coreProduct.
+FOUNDATION_EXPORT double coreProductVersionNumber;
+
+//! Project version string for coreProduct.
+FOUNDATION_EXPORT const unsigned char coreProductVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <coreProduct/PublicHeader.h>
+
+
